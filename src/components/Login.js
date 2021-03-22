13540a93fe48ca9ade6425530cc12cc6ccc9517d
Justin Z <contactjustinzamora@gmail.com>
2021-03-22 10:15:59 -0700

    Login form implemented

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Login = () => {
+    const INITIAL_STATE = {
+        username: "",
+        password: ""
+    }
+    const [formData, setFormData] = useState(INITIAL_STATE)
+
+    const handleChange  = () => {
+        const {name, value} = e.target
+        setFormData(state => ({...state, [name]:value}))
+    }
     return (
         <div>
-        Our Login Page
+        <form>
+            <label htmlFor="username">Username:</label>
+            <input
+            id="username"
+            name="username"
+            value={formData.username}
+            placeholder="Username"
+            onChange={handleChange}
+            />
+            <label htmlFor="password">Password</label>
+            <input
+            id="password"
+            name="password"
+            value={formData.password}
+            placeholder="Password"
+            onChange={handleChange}
+            />
+        </form>
         </div>
     )
 }
