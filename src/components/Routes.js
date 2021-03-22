6901c540881a1bfd6bcf92b6c3d9408f67fbdfab
Justin Z <contactjustinzamora@gmail.com>
2021-03-21 20:54:25 -0700

    Routes set up

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import {Switch, Route} from 'react-router-dom'
+import Home from './Home'
+import Login from './Login'
+import SignUp from './SignUp'
+import NotFound from './NotFound'
 
 const Routes = () => {
     return (
         <div>
-            Our Routes
+            <Switch>
+                <Route exact path="/login">
+                    <Login/>
+                </Route>
+
+                <Route exact path="/signup">
+                    <SignUp/>
+                </Route>
+
+                <Route exact path="/">
+                    <Home/>
+                </Route>
+                
+                <Route>
+                    <NotFound/>
+                </Route>
+            </Switch>
         </div>
     )
 }
