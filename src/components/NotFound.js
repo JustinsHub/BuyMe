0721e1a359b3a299e8885294b17515424858c3d1
Justin Z <contactjustinzamora@gmail.com>
2021-03-22 00:38:19 -0700

    Sign Up form implemented

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import {useHistory} from 'react-router-dom'
 
 const NotFound = () => {
+    const history = useHistory()
+
+    const goHome = () => {
+        history.push('/')
+    }
+
     return (
         <div>
             <h1>PAGE NOT FOUND</h1>
+            <button className="btn btn-primary" onClick={goHome}>Go Home</button>
         </div>
     )
 }
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const SignUp = () => {
+    const INITIAL_STATE = {
+        username: "",
+        password: "",
+        email: ""
+    }
+    const [formData, setFormData] = useState(INITIAL_STATE)
+
+    const handleChange = (e) => {
+        const {name, value} = e.target
+        setFormData(state => ({...state, [name]:value}))
+    }
     return (
         <div>
-            Our SignUp Page
+            <form>
+                <label htmlFor="username">Username:</label>
+                <input
+                id="username"
+                placeholder="username"
+                name="username"
+                value={formData.username}
+                onChange={handleChange}
+                />
+                <label htmlFor="password">Password:</label>
+                <input
+                id="password"
+                placeholder="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                />
+                <label htmlFor="email">Email:</label>
+                <input
+                id="email"
+                placeholder="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                />
+                <button>Sign Up</button>
+            </form>
         </div>
     )
 }
