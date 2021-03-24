65ab11775c4b2a01998d3b72f9ed58963489018e
Justin Z <contactjustinzamora@gmail.com>
2021-03-24 11:44:18 -0700

    added backend error handling

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,15 @@
 const express = require('express')
 
 const app = express()
+
+app.use((error, req, res, next)=> {
+    let status = error.status || 500
+    let message = error.message
+    return res.status(status).json({
+        error:{message, status}
+    })
+})
+
+app.listen(3000, ()=> {
+    console.log('Server on PORT 3000')
+})
