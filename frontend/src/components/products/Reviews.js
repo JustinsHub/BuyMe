24525c2798bd38389d764d71cd0b181fb5b27d4f
Justin Z <contactjustinzamora@gmail.com>
2021-05-25 09:30:41 -0700

    added carousel set up for Home component

--- frontend/src/components/products/Reviews.js
+++ frontend/src/components/products/Reviews.js
@@ -1,12 +1,31 @@
 import React from 'react'
+import '../styles/Reviews.css'
 
 const Reviews = () => {
-    //create a scrollable mock 5 star review with mock names
-    //find on bootstrap
     return (
-        <div>
-            <h1>What People Are Saying</h1>
+    <div>
+    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                foo
+            </div>
+            <div class="carousel-item">
+                hello
+            </div>
+            <div class="carousel-item">
+                bro
+            </div>
         </div>
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+    </div>
     )
 }
 
