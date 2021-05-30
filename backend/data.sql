ecfa21dba3b80e7f6f2751b1d3fc9324e7fb462d
Justin Z <contactjustinzamora@gmail.com>
2021-05-29 17:56:58 -0700

    added purchases, signature_meal, pair_meal tables

--- backend/data.sql
+++ backend/data.sql
@@ -1,20 +1,39 @@
-DROP DATABASE IF EXISTS BuyMe;
+DROP DATABASE IF EXISTS Pickout;
 
-CREATE DATABASE BuyMe;
+CREATE DATABASE Pickout;
 
-\c BuyMe;
+\c Pickout;
 
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
     id serial PRIMARY KEY,
-    username text UNIQUE NOT NULL,
-    password text NOT NULL,
-    first_name text,
-    last_name text,
-    email text NOT NULL,
-    address text
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    first_name TEXT,
+    last_name TEXT,
+    email TEXT NOT NULL,
+    address TEXT
+    created_on TIMESTAMP NOT NULL
 );
+
+CREATE TABLE signature_meal {
+    id serial PRIMARY KEY
+}
+
+CREATE TABLE pair_meal {
+    id serial PRIMARY KEY
+}
+
+CREATE TABLE purchases (
+    id serial PRIMARY KEY,
+    user_id INTEGER 
+        REFERENCES users ON DELETE CASCADE
+    signature_meal INTEGER 
+        REFERENCES signature_meal ON DELETE CASCADE
+    pair_meal INTEGER 
+        REFERENCES pair_meal ON DELETE CASCADE
+)
 
 INSERT INTO users (username, password, first_name, last_name, email, address) 
 VALUES ('Someone', 'yolobolo', 'dexter', 'wexter', 'dexter@wexter.com', '1227 Dog st. #445, Los Angeles, CA 90068')
