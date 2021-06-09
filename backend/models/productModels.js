ad157f9ae0b5e1ebb0b0d8fdc51bf45458e1b345
Justin Z <contactjustinzamora@gmail.com>
2021-06-08 22:37:29 -0700

    implemented purchase product API

--- backend/models/productModels.js
+++ backend/models/productModels.js
@@ -2,18 +2,17 @@ const db = require('../db')
 const ExpressError = require('../expressError')
 
 const timePurchased = new Date(Date.now())
-
-//add errors
 //precheck for userId
-
+//add error handling
 class Product {
     //Both queries add to DB when meal is purchased depending on which choice. (Can be both?)
+    
     //signature Meal query
     static async signatureMealPurchase(userId, mealId) {
         await db.query(
             //must match the exact DB names when inserting
-            `INSERT INTO purchases (user_id, signature_meal)
-            VALUES ($1, $2)`, [userId, mealId]
+            `INSERT INTO purchases (user_id, signature_meal, purchased_on)
+            VALUES ($1, $2, $3)`, [userId, mealId, timePurchased]
         )
     }
 
@@ -21,7 +20,7 @@ class Product {
     static async pairMealPurchase(userId, pairId){
         await db.query(
             `INSERT INTO purchases (user_id, pair_meal, purchased_on)
-            VALUES ($1, $2)`, [userId, pairId, timePurchased]
+            VALUES ($1, $2, $3)`, [userId, pairId, timePurchased]
         )
     }
 }
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,11 +2,22 @@ const express = require('express')
 const router = new express.Router()
 const Product = require('../models/productModels')
 
-router.post('/purchase/:userId/:mealId', async(req, res, next) => {
+//add error handling
+router.post('/signature/:mealId/purchase/:userId/', async(req, res, next) => {
     try{
         const {userId, mealId} = req.params
         await Product.signatureMealPurchase(userId, mealId)
-        return res.json({purchased: mealId})
+        return res.json({purchased: "Signature Meal"})
+    }catch(e){
+        return next(e)
+    }
+})
+
+router.post('/pair-meal/:pairId/purchase/:userId', async(req, res, next) => {
+    try{
+        const {userId, pairId} = req.params
+        await Product.pairMealPurchase(userId, pairId)
+        return res.json({purchased: "Pair Meal"})
     }catch(e){
         return next(e)
     }
